5ac67e5e3c01733d3d140b2c00771e98aed2dd36
Yoshitsugu Sekine <yoshitsugu.sekine@offisis.co.jp>
2022-07-14 15:06:43 +0900

    Add a process

--- app/src/main/java/jp/techacademy/yoshitsugu/ch0510timer/MainActivity.kt
+++ app/src/main/java/jp/techacademy/yoshitsugu/ch0510timer/MainActivity.kt
@@ -2,10 +2,28 @@ package jp.techacademy.yoshitsugu.ch0510timer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private var mTimer: Timer? = null
+    private var mTimerSec = 0.0
+    private var mHandler = Handler()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val timer = findViewById<TextView>(R.id.timer)
+
+        mTimer = Timer()
+        mTimer!!.schedule(object: TimerTask(){
+            override fun run (){
+                mTimerSec += 0.1
+                mHandler.post {
+                    timer.text = String.format("%.1f", mTimerSec)
+                }
+            }
+        }, 100,100)
     }
 }
